1bf8e7952bd38c4b439066cf201948e7d4e8766b
esanz <eduardo.sanz@nahitekdigital.com>
2022-07-18 10:40:51 +0200

    first commit

---
+++ src/main/java/com/atlassian/amatech/myPlugin/api/MyPluginComponent.java
@@ -0,0 +1,6 @@
+package com.atlassian.amatech.myPlugin.api;
+
+public interface MyPluginComponent
+{
+    String getName();
+}
---
+++ src/main/java/com/atlassian/amatech/myPlugin/impl/MyPluginComponentImpl.java
@@ -0,0 +1,25 @@
+package com.atlassian.amatech.myPlugin.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.atlassian.amatech.myPlugin.api.MyPluginComponent;
+
+
+public class MyPluginComponentImpl implements MyPluginComponent
+{
+        private final ApplicationProperties applicationProperties;
+
+        public MyPluginComponentImpl(final ApplicationProperties applicationProperties)
+    {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName()
+    {
+        if(null != applicationProperties)
+        {
+            return "myComponent:" + applicationProperties.getDisplayName();
+        }
+        
+        return "myComponent";
+    }
+}
